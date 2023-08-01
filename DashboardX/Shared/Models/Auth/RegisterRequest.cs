0333c643a5adae90edc23a09b37a1bcf4c00b1bd
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2023-08-01 18:36:16 +0200

    fix(register) - not validating data before register click

--- DashboardX/Shared/Models/Auth/RegisterRequest.cs
+++ DashboardX/Shared/Models/Auth/RegisterRequest.cs
@@ -2,16 +2,16 @@
 
 public class RegisterRequest
 {
-    [Required, MinLength(3), MaxLength(30)]
+    [JsonPropertyName("username"), Required, MinLength(3), MaxLength(30)]
     public string Username { get; set; } = string.Empty;
 
-    [Required, MinLength(6), MaxLength(30),
+    [JsonPropertyName("password"), Required, MinLength(6), MaxLength(30),
         RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "The password must contain at least one lowercase letter, one uppercase letter, and one digit.")]
     public string Password { get; set; } = string.Empty;
 
-    [Required, MinLength(6), MaxLength(30), Compare(nameof(Password), ErrorMessage = "Passwords do not match!")]
+    [ Required, MinLength(6), MaxLength(30), Compare(nameof(Password), ErrorMessage = "Passwords do not match!")]
     public string ConfirmPassword { get; set; } = string.Empty;
 
-    [Required, MinLength(6), MaxLength(256), EmailAddress]
+    [JsonPropertyName("email"), Required, MinLength(6), MaxLength(256), EmailAddress]
     public string Email { get; set; } = string.Empty;
 }
