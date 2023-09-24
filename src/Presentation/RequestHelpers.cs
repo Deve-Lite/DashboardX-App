d0fa516e99f38473044cab11e5cfe09fca0a80be
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2023-09-24 13:35:32 +0200

    feat(dialogs loading refactor)

--- src/Presentation/RequestHelpers.cs
+++ src/Presentation/RequestHelpers.cs
@@ -2,35 +2,48 @@
 
 public class RequestHelpers
 {
-    public static async Task InvokeAfterRequest(ISnackbar snackbar, IResult result, Func<Task> onSuccess, bool displayErrors = true)
+    public static async Task<bool> InvokeAfterRequest(ISnackbar snackbar, IResult result, Func<Task> onSuccess, bool displayErrors = true)
     {
         if ((Result)result)
         {
             await onSuccess.Invoke();
+            return true;
         }
-        else
-        {
-            if (!displayErrors)
-                return;
 
+        if (displayErrors)
             foreach (var error in result.Messages)
                 snackbar.Add(error, Severity.Error);
-        }
+
+        return false;
     }
 
-    public static void InvokeAfterRequest(ISnackbar snackbar, IResult result, string successMessage = "", bool displayErrors = true)
+    public static bool InvokeAfterRequest(ISnackbar snackbar, IResult result, Action onSuccess, bool displayErrors = true)
+    {
+        if ((Result)result)
+        {
+            onSuccess.Invoke();
+            return true;
+        }
+
+        if (displayErrors)
+            foreach (var error in result.Messages)
+                snackbar.Add(error, Severity.Error);
+
+        return false;
+    }
+
+    public static bool InvokeAfterRequest(ISnackbar snackbar, IResult result, string successMessage = "", bool displayErrors = true)
     {
         if ((Result)result)
         {
             snackbar.Add(successMessage, Severity.Success);
+            return true;
         }
-        else
-        {
-            if (!displayErrors)
-                return;
 
+        if (displayErrors)
             foreach (var error in result.Messages)
                 snackbar.Add(error, Severity.Error);
-        }
+
+        return false;
     }
 }
