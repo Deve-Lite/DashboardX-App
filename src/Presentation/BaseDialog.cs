f155f1111a1a7bab4e4bc16e0844e6194c3aa7d5
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2023-11-22 21:46:31 +0100

    feat(ui optimalization)

--- src/Presentation/BaseDialog.cs
+++ src/Presentation/BaseDialog.cs
@@ -2,14 +2,19 @@
 
 namespace Presentation;
 
-public class BaseDialog : MudDialog
+public class BaseDialog : MudDialog, IBrowserViewportObserver, IAsyncDisposable
 {
+    [Inject]
+    public IBrowserViewportService? BrowserViewportService { get; set; }
+
     [CascadingParameter]
     public MudDialogInstance? Dialog { get; set; }
 
-    protected bool isLoading = false;
+    public Guid Id { get; } = Guid.NewGuid();
+    public ResizeOptions ResizeOptions { get; } = new() { NotifyOnBreakpointOnly = false };
 
-    public bool IsLoading
+    private bool isLoading = false;
+    protected bool IsLoading
     {
         get
         {
@@ -29,5 +34,47 @@ public class BaseDialog : MudDialog
         }
     }
 
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender && IsBrowserViewportsEventsAvailable())
+        {
+            await BrowserViewportService!.SubscribeAsync(this, true);
+            await UpdateDialogOptions( await BrowserViewportService.GetCurrentBreakpointAsync());
+            await InvokeAsync(StateHasChanged);
+        }
+
+        await base.OnAfterRenderAsync(firstRender);
+    }
+
     public void Cancel() => Dialog!.Cancel();
+
+    public async ValueTask DisposeAsync()
+    {
+        if(IsBrowserViewportsEventsAvailable()) 
+            await BrowserViewportService?.UnsubscribeAsync(this)!;
+    }
+    public async Task NotifyBrowserViewportChangeAsync(BrowserViewportEventArgs browserViewportEventArgs)
+    {
+        await UpdateDialogOptions(browserViewportEventArgs.Breakpoint);
+    }
+
+    protected virtual bool IsBrowserViewportsEventsAvailable() => true;
+
+    protected virtual async Task UpdateDialogOptions(Breakpoint breakpoint)
+    {
+        var options = Dialog!.Options;
+
+        switch (breakpoint)
+        {
+            case Breakpoint.Xs:
+                options.FullScreen = true;
+                break;
+            default:
+                options.FullScreen = false;
+                break;
+        }
+
+        Dialog.SetOptions(options);
+        await InvokeAsync(StateHasChanged);
+    }
 }
--- src/Presentation/BaseFormDialog.cs
+++ src/Presentation/BaseFormDialog.cs
@@ -3,4 +3,7 @@
 public class BaseFormDialog : BaseDialog
 {
     protected MudForm Form = new();
+
+    protected override async Task OnAfterRenderAsync(bool firstRender) 
+        => await base.OnAfterRenderAsync(firstRender);
 }
